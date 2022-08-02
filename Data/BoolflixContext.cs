0e2b98da011ee86496d80e2ce0ba09aa543b3681
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-08-02 17:15:39 +0200

    make migrations and create DB

---
+++ Data/BoolflixContext.cs
@@ -0,0 +1,25 @@
+﻿using csharp_boolflix.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_boolflix.Data
+{
+    public class BoolflixContext : DbContext
+    {
+        public DbSet<VideoContent> VideoContents { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Profile> Profiles { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Playlist> Playlists { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db-boolflix;Integrated Security=True");
+        }
+    }
+
+}
---
+++ Migrations/20220802130630_AddCoverImageAndDescription.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace csharp_boolflix.Migrations
+{
+    public partial class AddCoverImageAndDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CoverImage",
+                table: "VideoContents",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "VideoContents",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverImage",
+                table: "VideoContents");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "VideoContents");
+        }
+    }
+}
